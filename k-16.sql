a89e3418cdb70cd6bf7579a182537d7f1eb5473a
ruthw <rwustenberg@gmail.com>
2019-09-19 04:27:26 -0500

    160209

--- k-16.sql
+++ k-16.sql
@@ -54,3 +54,32 @@ SELECT * FROM places WHERE rating > 3 AND type < 3 AND id < 5
 1	Amazing Villa by the Ocean	150	5	Phuket	1	2
 2	Amazing View in the Jungle⇥	80	4	Phangan	2	1
 4	Cool flat in trendy area	80	5	Chiang Mai	2	4
+
+--160206
+SELECT id, title, price FROM places ORDER by price
+7	Cozy room by the beach	40
+6	Stylish room in city center	50
+2	Amazing View in the Jungle⇥	80
+4	Cool flat in trendy area	80
+1	Amazing Villa by the Ocean	150
+5	Villa with 360 Rooftop	160
+
+--160207
+SELECT * FROM places
+WHERE title LIKE "%Amazing%"
+1	Amazing Villa by the Ocean	150	5	Phuket	1	2
+2	Amazing View in the Jungle⇥	80	4	Phangan	2	1
+
+--160208
+SELECT id, title AS NAME, price AS AMOUNT, rating FROM places
+WHERE rating -4
+1	Amazing Villa by the Ocean	150	5
+4	Cool flat in trendy area	80	5
+5	Villa with 360 Rooftop	160	5
+6	Stylish room in city center	50	3
+
+--160209
+SELECT * FROM users
+WHERE id BETWEEN 2 AND 3
+2	Sarah Black	sarah@email.com
+3	Albert Philips	albert@email.com
